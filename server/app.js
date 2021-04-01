28d52a6ca692c76ed0fc28ddeeb9a445ad41ac71
Tee <junyuntee@gmail.com>
2021-04-01 19:34:36 +0800

    update dbconfig

--- server/app.js
+++ server/app.js
@@ -11,50 +11,26 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// var dbConfig = {
+//     server: "LAPTOP-DJ1HQ97V",
+//     database: "DatabaseMod",
+//     port: 1433,
+//     user:'test',
+//     password:'test',
+//     "synchronize": true,
+// }
+
 var dbConfig = {
-    server: "LAPTOP-DJ1HQ97V",
-    database: "DatabaseMod",
-    port: 1433,
-    user:'test',
-    password:'test',
-    "synchronize": true,
+  server: "csc2008database.database.windows.net",
+  database: "databaseproject",
+  port: 1433,
+  user:'database',
+  password:'Password123',
+  "synchronize": true,
 }
 
 module.exports = dbConfig;
 
-// function getEmp() {
-//     var conn = new sql.ConnectionPool(dbConfig);
-//     var req = new sql.Request(conn);
-
-//     conn.connect(function (err) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         req.query("SELECT * FROM Users", function(err, recordset) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 console.log(recordset);
-//             }
-//             conn.close();
-//         });
-//     })
-// }
-
-
-// function getEmp() {
-//     var conn = new sql.ConnectionPool(dbConfig);
-
-//     conn.connect(function (err) {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//     })
-// }
-
-// getEmp();
 
 //Function to connect to database and execute query
 
@@ -87,38 +63,6 @@ var executeQuery = function(res, query){
 
 //GET ALL ACTIVE USERS FOR PATHWAYS
 
-
-
-
-//POST API
-// app.post("/api/users", function(req , res){
-//   var dbConn = new sql.Connection(dbConfig);
-//   dbConn.connect().then(function () {
-//   var transaction = new sql.Transaction(dbConn);
-//   transaction.begin().then(function () {
-//     var request = new sql.Request(transaction);
-//           request.query("INSERT INTO [Users] (id,firstName,lastName,email,contactNo) VALUES (2, 'name', 'name', 'name@gmail.com', '98765432')")
-//     .then(function 	() {
-//       transaction.commit().then(function (resp) {
-//                   console.log(resp);
-//                   dbConn.close();
-//               }).catch(function (err) {
-//                   console.log("Error in Transaction Commit " + err);
-//                   dbConn.close();
-//               });
-//     }).catch(function (err) {
-//               console.log("Error in Transaction Begin " + err);
-//               dbConn.close();
-//           })
-//   }).catch(function (err) {
-//           console.log(err);
-//           dbConn.close();
-//       }).catch(function (err) {
-//       //12.
-//       console.log(err);
-//   });
-// });
-// });
 
 app.post("/api/users", (req,res) => {
   const id = req.body.id
@@ -159,37 +103,6 @@ app.post("/api/users", (req,res) => {
 
 
 
-//app.post("/api/users", function(req , res){
-    // var query = "INSERT INTO [Users] (id,firstName,lastName,email,contactNo) VALUES (2, 'name', 'name', 'name@gmail.com', '98765432');"
-    // // executeQuery (res, query);
-    // sql.connect(dbConfig, function (err) {
-    //   console.log("connectedfsafasdfsad")
-    //     if (err) {   
-    //         console.log("Error while connecting database :- " + err);
-    //         res.send(err);
-    //     }
-    //     else {
-    //       // create Request object
-    //       var request = new sql.Request();
-    //       // query to the database
-          
-    //       request.query(query, function (err, response) {
-    //           console.log(response)
-    //         if (err) {
-    //           console.log("Error while querying database :- " + err);
-    //           res.send(err);
-            
-    //           }
-    //           else {
-    //             console.log(response);
-    //             res.send(response);
-    //           }
-           
-    //       });
-    //     }
-    // });  
-//});
-
 app.get("/api/users", function(req, res){
     var query = "SELECT * FROM [Users]";
     // executeQuery(res, query);
@@ -225,14 +138,5 @@ app.listen(3001, () => {
 });
 
 
-
-
-// http.createServer(app).listen(3001, () => {
-//     console.log("hello")
-// });
-
-// http.createServer(app).get((res,req) => {
-//     res.send("hello world")
-// });
 
 
